723bf1eb342834895184bae7a8298a5f579d003e
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-06-29 14:45:17 -0400

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header className='col-md-6'>
+      <h1><Link to='/'>Space Mission</Link></h1>
+    </header>
+  )
+}
+
+export default Header
