9c85051a1c9920d0cee118114f1de9776d577149
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-07-01 20:55:10 -0400

    Add files via upload

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav className='col-md-6'>
+      <Link to='/mission'>Mission</Link>
+      <Link to='/gallery'>Gallery</Link>
+      <Link to='/contact'>Contact</Link>
+    </nav>
+  )
+}
+
+export default Navbar
